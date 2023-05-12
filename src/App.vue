c0cee937ef2cdf0f52a2ce67d2c2e380db413dce
Dmitry Sakovich <sakovich.dima95@gmail.com>
2023-05-12 17:23:22 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+const tg = window.Telegram.WebApp
+
+const user = tg.initDataUnsafe.user
+
+const pay = () => {
+  const data = 'pay'
+  tg.sendData(JSON.stringify(data));
+}
+
+</script>
+
+<template>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <h1>Hello, {{ user?.username || 'my dear' }}!</h1>
+  <h3 v-if="user?.id">your id is: {{ user?.id }}</h3>
+  <h5>sereja pid0r</h5>
+  <button @click="pay">pay</button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
