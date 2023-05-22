4168a71c7d3a1d970c0c83cae18fbf3b500f530d
Dmitry Sakovich <sakovich.dima95@gmail.com>
2023-05-22 12:32:20 +0300

    add deploy script

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,6 +2,7 @@
     <div class="body">
         <div class="container">
             <h3>404</h3>
+            <h5><a href="/">На главную</a></h5>
         </div>
     </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    cors: false,
-  },
-  preview: {
-    port: 8080,
-    cors: false,
-  },
+
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    base: mode === 'development' ? '/' : '/chatGPT-web-app/',
+    server: {
+      port: 3000,
+      cors: false,
+    },
+    preview: {
+      port: 8080,
+      cors: false,
+    },
+  }
+
 })
