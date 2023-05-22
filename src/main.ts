ee46842d8c3e07dd495ab9510c233413af9dc505
Dmitry Sakovich <sakovich.dima95@gmail.com>
2023-05-22 17:43:57 +0300

    sss

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,5 @@ import App from './App.vue'
 
 const app = createApp(App)
 export const Tg = window.Telegram.WebApp||{}
+console.log('window.location.pathname', window.location.pathname)
 app.mount('#app')
